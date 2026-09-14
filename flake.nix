1950b90a44c1c0bea0cd819c8e1ebdab3687041f
keithmo <75361919+keithxc@users.noreply.github.com>
2026-09-14 12:44:40 +0800

    Prepare 0.2.3 session settings prerelease

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           };
         in pkgs.moonlight-qt.overrideAttrs (old: {
           pname = "deskport";
-          version = "0.2.2";
+          version = "0.2.3";
           src = pkgs.lib.cleanSourceWith {
             src = pkgs.lib.cleanSource self;
             # Documentation and CI edits do not change the client binary.
